13ed167db29f15f36ea69c1c643e78a54a0cea1c
Vidar <vidar.swenning@nexile.se>
2025-04-18 15:22:46 +0200

    Replace faulty toll calculation

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -20,29 +20,49 @@ namespace TollFeeCalculator
             if (vehicle.IsTollFree())
                 return 0;
 
-            DateTime intervalStart = dates[0];
-            int totalFee = 0;
-            foreach (DateTime date in dates)
-            {
-                int nextFee = GetTollFee(date);
-                int tempFee = GetTollFee(intervalStart);
+            /*  From the requirements:
+                  A vehicle should only be charged once an hour
+                      - In the case of multiple fees in the same hour period, the highest one applies.
+            
+                However, this can be interpreted in many different ways.
 
-                long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-                long minutes = diffInMillies / 1000 / 60;
+                To visualize this let's consider a vehicle incurring a fee every 40 minutes throughout the day
+                and let's imagine the individual tolls would be the following:
+                A: 8
+                B: 8
+                C: 13
+                D: 18
+                E: 13
+                F: 8
 
-                if (minutes <= 60)
-                {
-                    if (totalFee > 0) totalFee -= tempFee;
-                    if (nextFee >= tempFee) tempFee = nextFee;
-                    totalFee += tempFee;
-                }
-                else
-                {
-                    totalFee += nextFee;
-                }
-            }
-            if (totalFee > 60) totalFee = 60;
-            return totalFee;
+                If we let the time frame extend (as in the TimeFrameVariant.Extending implementation) since only
+                40 minutes has passed since the last fee, all fees would be considered to be within the same hour.
+                This would result in the vehicle only being charged for D for a total of 18.
+
+                If we instead set a fixed time frame (as in the TimeFrameVariant.Fixed implementation) the vehile
+                would be charged for multiple passings. Once the vehicle reaches C more than an hour has passed since
+                A so a new fee is incurred. However since there's only 40 minutes to D that higher fee is used for
+                this hourly period. The vehicle then incurrs a fee for E as well since more than an hour has passed
+                since C where the period started. The vehicle will therefore be charged for A, D and E for a total
+                of 39.
+
+                The first approach lets the vehicle skip payment by merging all instances into a single "hour" but
+                the second approach technically charged it for two tolls only 40 minutes apart. We can address this
+                with another variant (as in the TimeFrameVariant.ExtendingOnHigherFee implementation) where we only
+                consider the time since the actual charge. This would incurr payments at A, D and F for a total of
+                34.
+
+                While this third approach looks good given this example, it's possible to think up a scenario where
+                the resulting fee doesn't seem to match "what one would expect". For instance, if the vehicle
+                never appeared at A, we would only charge for D and F. This means B would be skipped despite being 
+                80 minutes before the first actual charge. However, charging for B would go against the requirement
+                since C is within the hour with a higher fee, but we can't charge for C since it's within 40 minutes
+                of D.
+
+                In conclusion, the requirement of "A vehicle should only be charged once an hour" is rather unclear.
+            */
+
+            return GetTotalFee(dates, 60, TimeFrameVariant.ExtendingOnHigherFee);
         }
 
         public static int GetTollFee(DateTime date)
@@ -75,6 +95,50 @@ namespace TollFeeCalculator
             // TODO: Figure out bank holidays
 
             return false;
+        }
+
+
+        private enum TimeFrameVariant
+        {
+            Fixed,
+            Extending,
+            ExtendingOnHigherFee,
+        }
+
+        private static int GetTotalFee(DateTime[] dates, int max, TimeFrameVariant variant)
+        {
+            int total = 0;
+            int prevFee = 0;
+            DateTime? prevTime = null;
+
+            foreach (DateTime date in dates)
+            {
+                int fee = GetTollFee(date);
+                if (fee == 0)
+                    continue;
+
+                if (!prevTime.HasValue || (date - prevTime.Value).TotalHours > 1)
+                {
+                    total += fee;
+                    prevFee = fee;
+                    prevTime = date;
+                }
+                else if (fee > prevFee)
+                {
+                    total += fee - prevFee;
+                    prevFee = fee;
+                    if (variant != TimeFrameVariant.Fixed)
+                        prevTime = date;
+                }
+                else if (variant == TimeFrameVariant.Extending)
+                    prevTime = date;
+
+
+                if (total >= max)
+                    return max;
+            }
+
+            return total;
         }
     }
 }
